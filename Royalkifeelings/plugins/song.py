4c9fe6f0f625a97eeef08559a8bb517d1acf7db3
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-11-19 21:09:53 +0530

    Update song.py

--- Royalkifeelings/plugins/song.py
+++ Royalkifeelings/plugins/song.py
@@ -59,7 +59,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ @royalkifeelings ❤**"
+        rep = f"**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ @ll_JATT_USTAAD_ll ❤**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul

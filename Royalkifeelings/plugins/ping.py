fc241a8388538ae4c64a95f5b1878d4958d685fb
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-11-19 21:07:48 +0530

    Update ping.py

--- Royalkifeelings/plugins/ping.py
+++ Royalkifeelings/plugins/ping.py
@@ -7,7 +7,7 @@ from Royalkifeelings import bot as Royalboyamit
 @Royalboyamit.on_message(filters.command(["ping"], ["/", ".", "!"]))
 async def ping(Client, message):
     start = datetime.now()
-    loda = await message.reply_text("**» 𝐊ᴀᴀʟ 𝐌ᴜsɪᴄ**")
+    loda = await message.reply_text("**» BAD MUSIC**")
     end = datetime.now()
     mp = (end - start).microseconds / 1000
     await loda.edit_text(f"**🦋 𝒀𝒕 🌷 𝑴𝒖𝒔𝒊𝒄 🍀 𝑩𝒐𝒕 🥀 𝑰𝒔 🌺 𝑹𝒖𝒏𝒏𝒊𝒏𝒈 💐 𝑵𝒐𝒘 🍁 𝑷𝒚𝑻𝒈-𝑪𝒂𝒍𝒍 🍃** `{mp} ms`")
